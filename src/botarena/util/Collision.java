8d298613f39cbd219deb310e02dd3d9e01fc7823
Lucas Hereld <duckman@piratehook.com>
2010-07-30 11:03:19 -0700

    Somehow this wasn't commited...

---
+++ src/botarena/util/Collision.java
@@ -0,0 +1,39 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package botarena.util;
+
+/**
+ *
+ * @author lucashereld
+ */
+public class Collision
+{
+    private Thing thing = null;
+    private int x;
+    private int y;
+
+    public Collision(Thing thing,int x,int y)
+    {
+        this.thing = thing;
+        this.x = x;
+        this.y = y;
+    }
+
+    public Thing getThing()
+    {
+        return thing;
+    }
+
+    public int getX()
+    {
+        return x;
+    }
+
+    public int getY()
+    {
+        return y;
+    }
+}
